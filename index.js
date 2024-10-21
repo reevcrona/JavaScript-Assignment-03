6104f184b03ac2ae92684f2adabe519de1f93cae
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-10-21 12:00:35 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const username = prompt("Please enter your username");
+const password = prompt("Please enter your password");
+
+
+function isValidPassword(uName,pWord){
+    
+    if(pWord.length >= 8 && !pWord.includes(" ") && pWord !== uName){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+console.log(isValidPassword(username,password));
